1f72a91e724e708fb0c9b55c97ede333db25fc95
cookie-monster- <ilive4legos+github@gmail.com>
2018-07-13 23:31:46 -0500

    IRI friday

--- main/Constants.java
+++ main/Constants.java
@@ -3,7 +3,7 @@ package main;
 public class Constants {
 	public static final double WHEELBASE = 26.96;
 	public static final double TIMESTEP = 0.01;
-	public static final double ACC_MAX = 6.0;
-	public static final double VEL_MAX = 8.0;
-	public static final String FILEPATH = "C:/Users/Drew/Desktop/2018offseasonPathGen/paths/";
+	public static final double ACC_MAX = 8.0;//6
+	public static final double VEL_MAX = 10.0;//8
+	public static final String FILEPATH = "C:/Users/frc4587/Desktop/2018offseasonPathGen/paths/";
 }
--- main/Main.java
+++ main/Main.java
@@ -7,20 +7,23 @@ public class Main {
 		Arcs a = new Arcs();
 		
     //PYRAMID TO LEFT SCALE
-		a.drawArc(-170, -5.25, 0.0, 4.5);
-		l.drawStraightPath(-19.5, 4.5, 3.0);
-		a.drawArc(-100, -4.25, 3.0, 0.0);
+	/*	a.drawArc(155, -5.75, 0.0, 4.5);
+		l.drawStraightPath(-25.5, 4.5, 1.0);
+		a.drawArc(70, -5.5, 1.0, 2.0);
+		l.drawStraightPath(-1.5, 2.0, 0.0);
+		*/
 		
+		l.drawStraightPath(-6, 0.0, 0.0);
 /*	RIGHT SIDE TO RIGHT SCALE 90 DEGREES	
  * l.drawStraightPath(-22.5, 0.0, 3.0);
 		a.drawArc(90, -4.0, 3.0, 0.0);
 	*/	
 		Writer w = new Writer();
 		//w.printArrays();
-		w.writeFile("testArc");
+		w.writeFile("backupToDriverStation");
 		//w.writeFile("sideGearDownfieldPath0");
 		
-		Reader r = new Reader("testArc");
+		Reader r = new Reader("backupToDriverStation");
 		Example ex = new Example(Plot.data().xy(r.getX(), r.getY()));
 		//Example ex1 = new Example(Plot.data().xy(r.getLinex3(), r.getPosVelAcc()));
 	}
